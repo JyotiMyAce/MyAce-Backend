9f1d5d825b270e30ef92df6e02dc9616f421936c
Sushilmyacebazar <backend.myacebazar@gmail.com>
2025-06-27 11:20:03 +0530

    add response builder and status code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+     protected $errorStatus       = 500;
+    protected $successStatus     = 200;
+    protected $validationStatus  = 400;
+    protected $unauthStatus      = 401;
+    protected $notFoundStatus    = 404;
+    protected $invalidPermission = 403;
 }
