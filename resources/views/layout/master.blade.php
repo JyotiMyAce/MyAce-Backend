7b0a3ec08b58738e7373d79e57c3039cf517e3af
Sushilmyacebazar <backend.myacebazar@gmail.com>
2025-07-01 12:34:23 +0530

    admin login page

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,13 +1,14 @@
 @extends('layout.base')
+
 @section('master')
     <div class="container-scroller">
-        @php
-            $auth = auth()->user();
-        @endphp
         <div class="main-wrapper">
             <!-- partial -->
-            @include('layout.include.header')
-            @include('layout.include.sidebar')
+
+            @if (!\Route::is('admin-login'))
+                @include('layout.include.header')
+                @include('layout.include.sidebar')
+            @endif
             @yield('content')
         </div>
         <!-- main-panel ends -->
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\admin\AuthController;
 use App\Http\Controllers\admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
-Route::controller(HomeController::class)->group(function(){
-    Route::get('/', 'index')->name('index');
+Route::prefix('admin')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::get('/', 'index')->name('admin-login');
+    });
+    Route::controller(HomeController::class)->group(function () {
+        Route::get('/dashboard', 'index')->name('admin-dashboard');
+    });
 });
